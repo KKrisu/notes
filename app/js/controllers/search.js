dbf2fbab4d5d67bf34dbee5b84e4a693614aa966
Kris KK <krzysztof.kesy@gmail.com>
2014-07-27 10:29:38 +0200

    search form

--- app/js/controllers/search.js
+++ app/js/controllers/search.js
@@ -4,13 +4,40 @@ angular.module('notes')
 
     $scope.results = [];
 
-    $scope.form = {};
+    $scope.form = {
+        searchBy: 'any',
+        input: '',
+        search: function () {
+            $location.search('');
+            $location.search($scope.form.searchBy, $scope.form.input);
+        }
+    };
 
-    api.all('posts').getList($location.search()).then(function (data) {
-        pr('success', data);
-        $scope.results = data;
-    }, function () {
-        pr('fail', arguments);
-    });
+    $scope.searchByOptions = ['any', 'tag'];
+
+    $scope.currentSearchBy = 'any';
+
+    var updateSearchResults = function () {
+        api.all('posts').getList($location.search()).then(function (data) {
+            pr('success', data);
+            $scope.results = data;
+        }, function () {
+            pr('fail', arguments);
+        });
+    };
+
+    $scope.$watch(function () { return $location.search(); }, function () {
+        var currentSearch = $location.search();
+        var searchKey = Object.keys(currentSearch)[0];
+
+        if( searchKey !== $scope.currentSearchBy ||
+            currentSearch[searchKey] !== $scope.form.input
+        ) {
+            $scope.form.searchBy = searchKey;
+            $scope.form.input = currentSearch[searchKey];
+        }
+
+        updateSearchResults();
+    }, true);
 
 });
