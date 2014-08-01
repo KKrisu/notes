8bba77ba416e36d5c4ab7f54ff2784406d41987b
Kris KK <krzysztof.kesy@gmail.com>
2014-08-02 00:17:01 +0200

    autolinker and styles improvements

---
+++ app/js/filters/autolinker.js
@@ -0,0 +1,18 @@
+angular.module('notesFilters')
+.filter('autolinker', function ($sce) {
+    'use strict';
+
+    var autolinker = new window.Autolinker();
+
+    return function (input) {
+
+        if(!input) {
+            return '';
+        }
+
+        var output = autolinker.link(input.toString());
+
+        return $sce.trustAsHtml(output);
+
+    };
+});
