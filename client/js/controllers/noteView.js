d919d233ee1b1ee3bde2f85fd5d7f602a564d4bc
Krzysztof Kesy <krzysztof.kesy@gmail.com>
2014-09-11 22:36:58 +0200

    UI tweaks - glyphicons added, other minor improvements.

--- client/js/controllers/noteView.js
+++ client/js/controllers/noteView.js
@@ -1,8 +1,10 @@
 angular.module('notes')
-.controller('noteView', function ($scope, $route, api) {
+.controller('noteView', function ($scope, $route, api, constants) {
     'use strict';
 
     var id = $route.current.params.id;
+
+    $scope.constants = constants;
 
     api.one('posts', id).get().then(function (data) {
         $scope.note = data;
