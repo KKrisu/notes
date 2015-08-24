59686bb778e469a26e1f44a293517fa324087c4d
Krzysztof Kesy <krzysztof.kesy@gmail.com>
2015-08-24 22:53:55 +0200

    elastic textarea height

---
+++ client/js/directives/elastic.js
@@ -0,0 +1,26 @@
+// borrowed from http://stackoverflow.com/a/24090733/630409
+//
+// automatically updates textarea height
+//
+angular.module('notes')
+.directive('elastic', function($timeout) {
+    'use strict';
+
+    return {
+
+        restrict: 'A',
+
+        link: function($scope, element) {
+            $scope.initialHeight = $scope.initialHeight || element[0].style.height;
+
+            var resize = function() {
+                element[0].style.height = $scope.initialHeight;
+                element[0].style.height = '' + element[0].scrollHeight + 'px';
+            };
+
+            element.on('input change', resize);
+
+            $timeout(resize, 0);
+        }
+    };
+});
